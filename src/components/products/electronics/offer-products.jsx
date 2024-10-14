3793a6cfef69139a821a90f767ec50796e0931f7
fastestsunil <sunil141292@gmail.com>
2024-10-14 17:03:59 +0530

    latest deal added

--- src/components/products/electronics/offer-products.jsx
+++ src/components/products/electronics/offer-products.jsx
@@ -1,32 +1,36 @@
 'use client';
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 import Link from 'next/link';
 // internal
 import ProductItem from './product-item';
 import { useGetOfferProductsQuery } from '@/redux/features/productApi';
-import { ArrowRightLong, ShapeLine } from '@/svg';
+import { ArrowRightLong } from '@/svg';
 import ErrorMsg from '@/components/common/error-msg';
 import HomeOfferPrdLoader from '@/components/loader/home/home-offer-prd-loader';
 
 // slider setting
 const sliderSetting = {
-  slidesPerView: 3,
+  slidesPerView: 2,
   spaceBetween: 30,
   pagination: {
     el: '.tp-deals-slider-dot',
     clickable: true,
   },
+  navigation: {
+    nextEl: '.tp-deals-slider-button-next',
+    prevEl: '.tp-deals-slider-button-prev',
+  },
   breakpoints: {
     1200: {
-      slidesPerView: 3,
+      slidesPerView: 2,
     },
     992: {
       slidesPerView: 2,
     },
     768: {
-      slidesPerView: 2,
+      slidesPerView: 1,
     },
     576: {
       slidesPerView: 1,
@@ -43,6 +47,7 @@ const OfferProducts = () => {
     isError,
     isLoading,
   } = useGetOfferProductsQuery('electronics');
+
   // decide what to render
   let content = null;
 
@@ -60,50 +65,44 @@ const OfferProducts = () => {
     content = (
       <Swiper
         {...sliderSetting}
-        modules={[Pagination]}
+        modules={[Navigation, Pagination]}
         className="tp-product-offer-slider-active swiper-container"
       >
         {product_items.map((item, i) => (
           <SwiperSlide key={i}>
-            <ProductItem product={item} offer_style={true} />
+            <ProductItem product={item} offer_design={true} />
           </SwiperSlide>
         ))}
-
-        <div className="tp-deals-slider-dot tp-swiper-dot text-center mt-40"></div>
       </Swiper>
     );
   }
 
   return (
-    <>
-      <section className="tp-product-offer grey-bg-2 pt-70 pb-80">
-        <div className="container">
-          <div className="row align-items-end">
-            <div className="col-xl-4 col-md-5 col-sm-6">
-              <div className="tp-section-title-wrapper mb-40">
-                <h3 className="tp-section-title">Deal of The Day</h3>
-              </div>
-            </div>
-            <div className="col-xl-8 col-md-7 col-sm-6">
-              <div className="tp-product-offer-more-wrapper d-flex justify-content-sm-end p-relative z-index-1">
-                <div className="tp-product-offer-more mb-40 text-sm-end grey-bg-2">
-                  <Link href="/shop" className="tp-btn tp-btn-2 tp-btn-blue">
-                    View All Deals
-                    <ArrowRightLong />
-                  </Link>
-                  <span className="tp-product-offer-more-border"></span>
-                </div>
-              </div>
-            </div>
+    <section className="tp-product-offer pb-55">
+      <div className="container">
+        <div className="product-header">
+          <div className="product-header-left">
+            <h3 className="product-title">Latest Deals for This Week</h3>
+            <p className="product-subtitle">
+              Don't miss out on this weeks deals
+            </p>
           </div>
-          <div className="row">
-            <div className="col-xl-12">
-              <div className="tp-product-offer-slider fix">{content}</div>
+          <Link href="/shop" className="view-all-link">
+            View All <ArrowRightLong />
+          </Link>
+        </div>
+        <div className="row">
+          <div className="col-xl-12">
+            <div className="tp-product-offer-slider-wrapper">
+              {content}
+              <div className="tp-deals-slider-dot tp-swiper-dot text-center mt-40"></div>
+              <div className="tp-deals-slider-button-next tp-swiper-next"></div>
+              <div className="tp-deals-slider-button-prev tp-swiper-prev"></div>
             </div>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
