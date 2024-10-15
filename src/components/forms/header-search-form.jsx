67f4e6b41c360700cd7ad5b6870087db72a353f4
fastestsunil <sunil141292@gmail.com>
2024-10-15 16:53:17 +0530

    searc input

--- src/components/forms/header-search-form.jsx
+++ src/components/forms/header-search-form.jsx
@@ -1,49 +1,37 @@
 'use client';
 import { useState } from 'react';
 // internal
-import { Search } from '@/svg';
-import NiceSelect from '@/ui/nice-select';
 import useSearchFormSubmit from '@/hooks/use-search-form-submit';
 
 const HeaderSearchForm = () => {
-  const { setSearchText, setCategory, handleSubmit, searchText } =
-    useSearchFormSubmit();
-
-  // selectHandle
-  const selectCategoryHandle = e => {
-    setCategory(e.value);
-  };
+  const { setSearchText, handleSubmit, searchText } = useSearchFormSubmit();
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="tp-header-search-wrapper d-flex align-items-center">
-        <div className="tp-header-search-box">
-          <input
-            onChange={e => setSearchText(e.target.value)}
-            value={searchText}
-            type="text"
-            placeholder="Search for Products..."
-          />
-        </div>
-        <div className="tp-header-search-category">
-          <NiceSelect
-            options={[
-              { value: 'Select Category', text: 'Select Category' },
-              { value: 'electronics', text: 'electronics' },
-              // { value: "fashion", text: "fashion" },
-              // { value: "beauty", text: "beauty" },
-              // { value: "jewelry", text: "jewelry" },
-            ]}
-            defaultCurrent={0}
-            onChange={selectCategoryHandle}
-            name="Select Category"
-          />
-        </div>
-        <div className="tp-header-search-btn">
-          <button type="submit">
-            <Search />
-          </button>
-        </div>
+    <form onSubmit={handleSubmit} className="tp-header-search-form">
+      <div className="tp-header-search-wrapper">
+        <input
+          onChange={e => setSearchText(e.target.value)}
+          value={searchText}
+          type="text"
+          placeholder="Search for products..."
+          className="tp-header-search-input"
+        />
+        <button type="submit" className="tp-header-search-btn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+          </svg>
+        </button>
       </div>
     </form>
   );
