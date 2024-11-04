df11f3bb0a1347f20577bf258daaba04f87e5566
fastestsunil <sunil141292@gmail.com>
2024-11-04 16:48:36 +0530

    ui updated

--- src/components/hero-banner/home-hero-slider.jsx
+++ src/components/hero-banner/home-hero-slider.jsx
@@ -6,77 +6,36 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import Image from 'next/image';
 import Link from 'next/link';
 // internal
-import slider_img_1 from '@assets/img/slider/slider-img-1.png';
-import slider_img_2 from '@assets/img/slider/slider-img-2.png';
-import slider_img_3 from '@assets/img/slider/slider-img-3.png';
-import shape_1 from '@assets/img/slider/shape/slider-shape-1.png';
-import shape_2 from '@assets/img/slider/shape/slider-shape-2.png';
-import shape_3 from '@assets/img/slider/shape/slider-shape-3.png';
-import shape_4 from '@assets/img/slider/shape/slider-shape-4.png';
-import { ArrowRightLong, SliderNextBtn, SliderPrevBtn, TextShape } from '@/svg';
+import banner1 from '@assets/img/banner/slider/banner1.webp';
+import banner1Mobile from '@assets/img/banner/slider/banner1-mobile.webp';
+import { ArrowRightLong, SliderNextBtn, SliderPrevBtn } from '@/svg';
 
 // slider data
 const sliderData = [
   {
     id: 1,
-    pre_title: { text: 'Starting at', price: 274 },
-    title: 'The best tablet Collection 2023',
-    subtitle: {
-      text_1: 'Exclusive offer ',
-      percent: 35,
-      text_2: 'off this week',
-    },
-    img: slider_img_1,
-    green_bg: true,
+    title: 'Your One-Stop Shop for Reliable Auto Parts',
+    subtitle: 'Drive with Confidence, Drive with Quality',
+    img: banner1,
+    mobileImg: banner1Mobile,
   },
   {
     id: 2,
-    pre_title: { text: 'Starting at', price: 999 },
-    title: 'The best note book collection 2023',
-    subtitle: {
-      text_1: 'Exclusive offer ',
-      percent: 10,
-      text_2: 'off this week',
-    },
-    img: slider_img_2,
-    green_bg: true,
-  },
-  {
-    id: 3,
-    pre_title: { text: 'Starting at', price: 999 },
-    title: 'The best note book collection 2023',
-    subtitle: {
-      text_1: 'Exclusive offer ',
-      percent: 10,
-      text_2: 'off this week',
-    },
-    img: slider_img_3,
-    is_light: true,
+    title: 'Your One-Stop Shop for Reliable Auto Parts',
+    subtitle: 'Drive with Confidence, Drive with Quality',
+    img: banner1,
+    mobileImg: banner1Mobile,
   },
 ];
-
-function Shape({ img, num }) {
-  return (
-    <Image
-      className={`tp-slider-shape-${num}`}
-      src={img}
-      alt="slider-shape"
-      priority
-    />
-  );
-}
 
 const HomeHeroSlider = () => {
   const [active, setActive] = useState(false);
 
   // handleActiveIndex
   const handleActiveIndex = index => {
-    if (index === 2) {
-      setActive(true);
-    } else {
-      setActive(false);
-    }
+    setActive(index === 0);
   };
+
   return (
     <>
       <section className="tp-slider-area p-relative z-index-1 pb-55">
@@ -99,38 +58,31 @@ const HomeHeroSlider = () => {
           {sliderData.map(item => (
             <SwiperSlide
               key={item.id}
-              className={`tp-slider-item tp-slider-height d-flex align-items-center ${
-                item?.green_bg
-                  ? 'green-dark-bg'
-                  : item?.is_light
-                  ? 'is-light'
-                  : ''
-              }`}
-              style={{ backgroundColor: item.is_light && '#E3EDF6' }}
+              className="tp-slider-item tp-slider-height d-flex align-items-center"
+              // style={{
+              //   backgroundImage: `url(${item.img.src})`,
+              //   backgroundSize: 'cover',
+              //   backgroundPosition: 'center',
+              // }}
             >
-              <div className="tp-slider-shape">
-                <Shape img={shape_1} num="1" />
-                <Shape img={shape_2} num="2" />
-                <Shape img={shape_3} num="3" />
-                <Shape img={shape_4} num="4" />
-              </div>
+              <Image
+                src={item?.img?.src ?? ''}
+                alt="banner"
+                // loading="lazy"
+                // placeholder="blur"
+                fill
+                priority
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                className="object-fit-cover object-position-center"
+              />
               <div className="container">
                 <div className="row align-items-center">
                   <div className="col-xl-5 col-lg-6 col-md-6">
                     <div className="tp-slider-content p-relative z-index-1">
-                      <span>
-                        {item.pre_title.text} <b>${item.pre_title.text}</b>
-                      </span>
-                      <h3 className="tp-slider-title">{item.title}</h3>
-                      <p>
-                        {item.subtitle.text_1}
-                        <span>
-                          -{item.subtitle.percent}%
-                          <TextShape />
-                        </span>{' '}
-                        {item.subtitle.text_2}
-                      </p>
-
+                      <h3 className="tp-slider-title text-white">
+                        {item.title}
+                      </h3>
+                      <p className="text-white">{item.subtitle}</p>
                       <div className="tp-slider-btn">
                         <Link
                           href="/shop"
@@ -139,11 +91,6 @@ const HomeHeroSlider = () => {
                           Shop Now <ArrowRightLong />
                         </Link>
                       </div>
-                    </div>
-                  </div>
-                  <div className="col-xl-7 col-lg-6 col-md-6">
-                    <div className="tp-slider-thumb text-end">
-                      <Image src={item.img} alt="slider-img" />
                     </div>
                   </div>
                 </div>
