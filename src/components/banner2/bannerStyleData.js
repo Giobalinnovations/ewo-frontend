8d09267891fc9a31717ad9485dd3d86883d3b316
fastestsunil <sunil141292@gmail.com>
2024-11-08 15:49:01 +0530

    new banner added

--- src/components/banner2/bannerStyleData.js
+++ src/components/banner2/bannerStyleData.js
@@ -4,7 +4,8 @@ export const bannerDataHome2 = [
     badge: 'On Sale This Week',
     title: 'Drive with Confidence',
     text: 'Find quality auto parts that can enhance performance, safety, and comfort',
-    image: '/assets/img/banner/styles/banner-01.jpg',
+    image:
+      'https://res.cloudinary.com/datdyxl7o/image/upload/c_fill,w_359,h_300/v1731060158/banner-style-1_lt64rp.jpg',
     buttonLink: '/shop',
     buttonText: 'Shop Now',
     isDark: false,
@@ -13,7 +14,8 @@ export const bannerDataHome2 = [
     badge: 'On Sale This Week',
     title: 'Quality Auto Parts at Your Fingertips',
     text: 'Premium quality auto parts to experience a smooth ride and utmost power-all under one roof.',
-    image: '/assets/img/banner/styles/banner-01.jpg',
+    image:
+      'https://res.cloudinary.com/datdyxl7o/image/upload/c_fill,w_359,h_300/v1731060158/banner-style-3_dmpoal.jpg',
     buttonLink: '/shop',
     buttonText: 'Explore Our Collection',
     isDark: true,
@@ -22,19 +24,21 @@ export const bannerDataHome2 = [
     badge: 'Special Offer',
     title: 'Elevate your driving experience',
     text: 'Unlock maximum performance and reliability with our trusted auto parts. Each part you require, made to last.',
-    image: '/assets/img/banner/styles/banner-01.jpg',
+    image:
+      'https://res.cloudinary.com/datdyxl7o/image/upload/c_fill,w_359,h_300/v1731060158/banner-style-2_np00d6.jpg',
     buttonLink: '/shop',
     buttonText: 'Shop Quality Parts',
     isDark: false,
   },
 ];
-// Example usage:
+
 export const bannerDataHome3 = [
   {
     badge: 'On Sale This Week',
     title: 'Power Your Ride with Steller Parts',
     text: 'Quality auto parts to maintain your vehicle in excellent condition at its best.',
-    image: '/assets/img/banner/styles/banner-01.jpg',
+    image:
+      'https://res.cloudinary.com/datdyxl7o/image/upload/c_fill,w_546,h_363/v1731060866/banner-style-4_mkcuna.jpg',
     buttonLink: '/shop',
     buttonText: 'Find Your Fit',
     isDark: false,
@@ -43,7 +47,8 @@ export const bannerDataHome3 = [
     badge: 'On Sale This Week',
     title: 'Essentials to Upgrade, you can Rely on.',
     text: 'From must-have components to performance enhancers, EWO has it all. Find premium parts crafted for endurance and power.',
-    image: '/assets/img/banner/styles/banner-01.jpg',
+    image:
+      'https://res.cloudinary.com/datdyxl7o/image/upload/c_fill,w_546,h_363/v1731061049/banner-style-5_jkv2nv.jpg',
     buttonLink: '/shop',
     buttonText: 'Shop the best part',
     isDark: true,
